d9b1e0e8152cf39737cd67997e712aa326b65f61
Hong <h6752153@naver.com>
2024-04-24 16:11:16 +0900

    aski2 제형

---
+++ src/아스키아트/googulMain.java
@@ -0,0 +1,16 @@
+package 아스키아트;
+
+public class googulMain {
+
+	public static void main(String[] args) {
+		
+		googul gg = new googul();
+		googul(gg);
+		
+	}
+	
+	public static void googul(googul gg) {
+		gg.Googul();
+	}
+
+}
